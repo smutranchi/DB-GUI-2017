4dd44bd9d0f6bb3e618fbe5fbba65d4011b33636
Seung Ki Lee <seungkilee92@gmail.com>
2017-11-18 14:02:34 -0600

    created all pages needed for functionality. Let's start coding boyz

---
+++ frontend/src/app/module/view-playlist/view-playlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-playlist',
+  templateUrl: './view-playlist.component.html',
+  styleUrls: ['./view-playlist.component.css']
+})
+export class ViewPlaylistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
