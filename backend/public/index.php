cb87e0771fc1e1ecde6299a83203c30c2c403702
Chi Tran <chit@smu.edu>
2017-11-22 02:52:34 +0000

    Get all playlist routes done

--- backend/public/index.php
+++ backend/public/index.php
@@ -24,8 +24,8 @@ require __DIR__ . '/../src/dependencies.php';
 require __DIR__ . '/../src/middleware.php';
 
 // Register routes
-require __DIR__ . '/../src/routes.php';
-
+//require __DIR__ . '/../src/routes.php';
+require __DIR__. '/../src/playlist.php';
 // Run app
 $app->run();
                    
