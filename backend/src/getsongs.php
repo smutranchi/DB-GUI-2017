36bfc948b0dfb26a03aeb5a21f562518efc24099
Chi Tran <chit@smu.edu>
2017-11-23 23:00:02 +0000

    Chi-Help-with-routes-PLAYLIST-SONGS

--- backend/src/getsongs.php
+++ backend/src/getsongs.php
@@ -3,7 +3,9 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+
 require __DIR__ . '/../vendor/autoload.php';
+
 //require __DIR__ . '/../classes/ClassVideos.php';
 //require __DIR__ . '/../classes/ClassUsers.php';
 //require __DIR__ . '/../classes/ClassUsersLikeVideos.php';
@@ -21,7 +23,7 @@ $app->get('/songs', function (Request $request, Response $response, array $args)
 });
 
 // Retrieve song with id 
-$app->get('/songs/[{id}]', function ($request, $response, $args) {
+$app->get('/songs/[{id}]', function (Request $request, Response $response, array $args) {
         $pdo = $this->db;
 	$sth = $pdo->prepare('SELECT l.song_id, l.url, a.active_id,
                                 a.user_id, a.playlist_id, a.likes
